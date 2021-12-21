30ba5934e994bcb75f6a4edea10afab4c6bf97fc
Wataru Okada <wataru.okada@flect.co.jp>
2021-12-22 08:20:23 +0900

    update

--- dist/index.js
+++ dist/index.js
@@ -20,13 +20,13 @@ const startServer = async () => {
     if (process.env.SLACK_SIGNING_SECRET && process.env.SLACK_CLIENT_ID && process.env.SLACK_CLIENT_SECRET && process.env.SLACK_STATE_SECRET) {
         console.log("SLACK FEDERATION MODE");
         const { server, receiver } = await (0, slackApp_1.startSlackApp)(port);
-        const STATIC_PATH = __dirname + "/../frontend/dist/";
-        receiver.app.use("/static", express_1.default.static(STATIC_PATH));
-        console.log(`[SLACK APP] static path: ${STATIC_PATH}`);
-        receiver.app.use(express_1.default.json());
-        (0, rest_1.setupRestApi)(receiver.app);
-        (0, chimeApi_1.setupChimeApi)(receiver.app);
-        (0, socketIO_1.setupSocketIO)(server);
+        // const STATIC_PATH = __dirname + "/../frontend/dist/";
+        // receiver.app.use("/static", express.static(STATIC_PATH));
+        // console.log(`[SLACK APP] static path: ${STATIC_PATH}`);
+        // receiver.app.use(express.json());
+        // setupRestApi(receiver.app);
+        // setupChimeApi(receiver.app);
+        // setupSocketIO(server);
     }
     else {
         console.log("NORMAL MODE");
